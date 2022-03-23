9d0965cc5679788884a0e010e592fe40aa733813
thor-cheese <Runethorchainnewbie@gmail.com>
2022-03-22 19:12:25 -0700

    test

--- marketplace_api/api/queriess.py
+++ marketplace_api/api/queriess.py
@@ -56,7 +56,7 @@ def getTreasury_resolver(obj, info,date):
 
     try:
 
-        date_record = [event.to_dict() for event in treasuries.query.filter_by(datestring=date).limit(limit).offset(offset).all()]
+        date_record = [event.to_dict() for event in treasuries.query.filter_by(datestring=date).all()]
         print(date)
         # date_record = treasuries.query.filter_by(datestring=date).first().to_dict()
 
@@ -64,7 +64,7 @@ def getTreasury_resolver(obj, info,date):
         print('date_record')
         print(date_record)
 
-        maxDateD = treasuries.query.order_by(treasuries.date.desc()).first().date
+
 
         maxDate = treasuries.query.order_by(treasuries.date.desc()).first()
 
